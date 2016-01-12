421cfb2dfc45bad472a550df96676e06313ace15
Björn Dahlgren <bjodah@gmail.com>
2016-01-12 18:25:46 +0100

    Use explicit argument for anaconda root in release.sh script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 # Usage:
 #
-#    $ ./scripts/release.sh v1.2.3
+#    $ ./scripts/release.sh v1.2.3 ~/anaconda2/bin
 #
 
 if [[ $1 != v* ]]; then
@@ -15,7 +15,7 @@ PKG=$(find . -maxdepth 2 -name __init__.py -print0 | xargs -0 -n1 dirname | xarg
 PKG_UPPER=$(echo $PKG | tr '[:lower:]' '[:upper:]')
 ./scripts/run_tests.sh
 env ${PKG_UPPER}_RELEASE_VERSION=$1 python setup.py sdist
-./scripts/build_conda_recipe.sh $1
+PATH=$2:$PATH ./scripts/build_conda_recipe.sh $1
 
 # All went well
 git tag -a $1 -m $1
