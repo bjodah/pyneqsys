036969c233786a4fc14a710760824e39f254c561
Björn Dahlgren <bjodah@gmail.com>
2016-11-08 17:14:15 +0100

    Make levmar report success==False when minima (not root) found

--- pyneqsys/core.py
+++ pyneqsys/core.py
@@ -434,20 +434,29 @@ generated/scipy.optimize.root.html
         warnings.warn("ipopt interface untested at the moment")
         return minimize_ipopt(self.f_callback, intern_x0, jac=self.j_callback)
 
-    def _solve_levmar(self, intern_x0, **kwargs):
+    def _solve_levmar(self, intern_x0, tol=1e-8, **kwargs):
         import warnings
         warnings.warn("levmar interface is provisional, may change.")
         import levmar
+
+        if 'eps1' in kwargs or 'eps2' in kwargs or 'eps3' in kwargs:
+            pass
+        else:
+            kwargs['eps1'] = kwargs['eps2'] = kwargs['eps3'] = tol
+
         def _f(*args):
             return np.asarray(self.f_callback(*args))
+
         def _j(*args):
             return np.asarray(self.j_callback(*args))
+
         _x0 = np.asarray(intern_x0)
         _y0 = np.zeros(self.nf)
         with warnings.catch_warnings(record=True) as wrns:
             warnings.simplefilter("always")
-            p_opt, p_cov, info = levmar.levmar(_f, _x0, _y0, jacf=_j, **kwargs)
-        success = len(wrns) == 0
+            p_opt, p_cov, info = levmar.levmar(_f, _x0, _y0, args=(self.internal_params,),
+                                               jacf=_j, **kwargs)
+        success = len(wrns) == 0 and np.all(np.abs(_f(p_opt, self.internal_params)) < tol)
         for w in wrns:
             raise w
         e2p0, (e2, infJTe, Dp2, mu_maxJTJii), nit, reason, nfev, njev, nlinsolv = info
--- pyneqsys/tests/test_levmar.py
+++ pyneqsys/tests/test_levmar.py
@@ -3,7 +3,9 @@ from __future__ import (absolute_import, division, print_function)
 
 import pytest
 
-from .test_core import _test_neqsys_no_params
+from .test_core import (
+    _test_neqsys_params, _test_neqsys_no_params, _test_fail
+)
 
 try:
     import levmar
@@ -12,5 +14,15 @@ except ImportError:
 
 
 @pytest.mark.skipif(levmar is None, reason='levmar package unavailable')
-def test_levmar():
+def test_levmar_neqsys_no_params():
     _test_neqsys_no_params('levmar', eps1=1e-10, eps2=1e-10, eps3=1e-10)
+
+
+@pytest.mark.skipif(levmar is None, reason='levmar package unavailable')
+def test_levmar_neqsys_params():
+    _test_neqsys_params('levmar', eps1=1e-10, eps2=1e-10, eps3=1e-10)
+
+
+@pytest.mark.skipif(levmar is None, reason='levmar package unavailable')
+def test_levmar_fail():
+    _test_fail('levmar')
